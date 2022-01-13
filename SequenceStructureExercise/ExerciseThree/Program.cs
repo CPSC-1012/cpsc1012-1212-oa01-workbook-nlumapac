5db6dad1324766fec944b91326d4524d21db7083
Niño Angelo Lumapac <nlumapac1@nait.ca>
2022-01-13 15:46:09 -0700

    Initial commit for Exercise 3

---
+++ SequenceStructureExercise/ExerciseThree/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Purpose: Computes the area of a hexagon
+ * 
+ * Input: Side of a hexagon
+ * 
+ * Output: Area
+ */
+using System;
+
+namespace ExerciseThree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user and read the value
+            Console.WriteLine("Enter the length of the side:");
+            double s = double.Parse(Console.ReadLine());
+
+            // Calculate the area
+            double area = (3 * Math.Sqrt(3) * Math.Pow(s,2))/2;
+
+            // Display the area
+            Console.WriteLine($"The area of the hexagon is {area:0.####}");
+        }
+    }
+}
