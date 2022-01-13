fbd95a5a0b99242730f1cfdf9db4b39fb4d2aba0
Niño Angelo Lumapac <nlumapac1@nait.ca>
2022-01-13 15:09:15 -0700

    Initial commit for Exercise 1

---
+++ SequenceStructureExercise/SequenceStructureExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SequenceStructureExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Nino Angelo, my age is 31 and I hope to earn $250,000.00 per year");
+        }
+    }
+}
