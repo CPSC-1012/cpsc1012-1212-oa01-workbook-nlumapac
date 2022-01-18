bf01bd1aa522c4a742970ea8c088e9a07c8e01cd
Niño Angelo Lumapac <nlumapac1@nait.ca>
2022-01-18 00:29:39 -0700

    Arithmetic problems item 4

---
+++ ArithmeticProblems/SumOfDigitsApp/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Purpose: To calculate the sum of the digits of a three-digit whole number
+ * 
+ * Input: three-digit whole number
+ * 
+ * Output: sum of the digits of the number
+ * 
+ * Written by: Nino Angelo Lumapac
+ * 
+ * Written for: Sam Wu
+ * 
+ * Section: A01
+ * 
+ * Algorith:    Step 1) Prompt and read in the number
+ *              Step 2) Extract right most digit from number
+ *                      digit1 =  number % 10
+ *              Step 3) Extract right most digit from number
+ *                      number = number / 10
+ *              Step 4) Extract right most digit from number
+ *                      digit2 =  number % 10
+ *              Step 5) Remoe right most digit from the number
+ *                      number = number / 10
+ *              Step 6) Extract right most digit from number
+ *                      digit3 = number % 10
+ *              Step 7) Compute the sum of the digits as follows:
+ *                      sum = digit1 + digit2 + digit3
+ *              Step 8) Display result
+ */
+using System;
+
+namespace SumOfDigitsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1) Prompt and read in the number
+            Console.Write("Enter a three-digit whole number: ");
+            int number = int.Parse(Console.ReadLine());
+            // Step 2) Extract right most digit from number
+            int digit1 = number % 10;
+            // Step 3) Extract right most digit from number
+            number = number / 10;
+            // Step 4) Extract right most digit from number
+            int digit2 = number % 10;
+            // Step 5) Remoe right most digit from the number
+            number = number / 10;
+            // Step 6) Extract right most digit from number
+            int digit3 = number % 10;
+            // Step 7) Compute the sum of the digits
+            int sum = digit1 + digit2 + digit3;
+            // Step 8) Display result
+            Console.WriteLine($"The sum of the digits is: {sum}");
+        }
+    }
+}
