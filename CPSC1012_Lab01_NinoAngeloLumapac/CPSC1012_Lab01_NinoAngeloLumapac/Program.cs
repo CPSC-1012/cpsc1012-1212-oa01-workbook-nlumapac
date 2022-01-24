6a9f506eb3fbb6e275c0004777ed788c68d9d24e
Niño Angelo Lumapac <nlumapac1@nait.ca>
2022-01-24 16:08:25 -0700

    Added instructor solution comments

--- CPSC1012_Lab01_NinoAngeloLumapac/CPSC1012_Lab01_NinoAngeloLumapac/Program.cs
+++ CPSC1012_Lab01_NinoAngeloLumapac/CPSC1012_Lab01_NinoAngeloLumapac/Program.cs
@@ -7,6 +7,30 @@
  * 
  * Output:  Subtotal of the purchase, Provincial sales tax, Federal sales tax,
  *          Total sales tax, and Total of the sale
+ *          
+ * Algorith:    1) Prompt and read in the values for itemPrice and itemQuantity
+ *              2) Compute the subTotal, provincialTax, federalTax, totalTax, and totalPrice
+ *                  subTotal = itemPrice * itemQuantity
+ *                  provincialTax = subTotal * 0.06
+ *                  federalTax = subTotal * 0.05
+ *                  totalTax = provincialTax + federalTax
+ *                  totalPrice = subTotal + totalTax
+ *              3) Display the subTotal, provincialTax, federalTax, totalTax, totalPrice
+ *              
+ * Test Plan:
+ *      Test Case       Test Data           ExpectedResults
+ *      ----------      -----------         -------------------
+ *      Sample data     itemPrice=6.99      subTotal=13.98
+ *                      itemQuantity=2      provincialTax=0.84
+ *                                          federalTax=0.70
+ *                                          totalTax=1.54
+ *                                          totalPrice=15.52
+ *                                          
+ *      High price      itemPrice=99.99     subTotal=1099.89
+ *      High quantity   itemQuantity=11     provincialTax=65.99
+ *                                          federalTax=54.99
+ *                                          totalTax=120.98
+ *                                          totalPrice=1220.87
  * 
  * Written by: Nino Angelo Lumapac
  * 
@@ -34,28 +58,28 @@ namespace CPSC1012_Lab01_NinoAngeloLumapac
             double quantity = double.Parse(Console.ReadLine());
 
             // Calculate the sub total of purchase
-            double subTotal = price * quantity;
+            double subTotal = price * quantity; // --> Math.Round(price * itemQuantity, 2);
             // Calculate the provincial sales tax
-            double provincialTax = subTotal * ProvincialTaxRate;
+            double provincialTax = subTotal * ProvincialTaxRate;    // --> Math.Round(subTotal * ProvincialTaxRate, 2);
             // Calculate the federal sales tax
-            double federalTax = subTotal * FederalTaxRate;
+            double federalTax = subTotal * FederalTaxRate;  // --> Math.Round(subTotal * FederaTaxRate, 2);
             // Calculate the total sales tax
-            double totalTax = provincialTax + federalTax;
+            double totalTax = provincialTax + federalTax;   // --> Math.Round(provincialTax + FederalTaxRate, 2);
             // Calculate the total of the sale
-            double totalPrice = subTotal + totalTax;
+            double totalPrice = subTotal + totalTax;    // --> Math.Round(subTotal + totalTax, 2);
 
             // Display the results
             Console.WriteLine("Invoice Summary");
             Console.WriteLine("=========================");
             Console.WriteLine($"{"Sub Total:",-16} {subTotal,8:F2}");
-            Console.WriteLine("\n");
+            Console.WriteLine("\n");    // --> Console.WriteLine();
             Console.WriteLine($"{"Provincial Tax:",-16} {provincialTax,8:F2}");
             Console.WriteLine($"{"Federal Tax:",-16} {federalTax,8:F2}");
             Console.WriteLine($"{"Total Tax:",-16} {totalTax,8:F2}");
-            Console.WriteLine("\n");
+            Console.WriteLine("\n");    // --> Console.WriteLine();
             Console.WriteLine($"{"Total Price:",-16} {totalPrice,8:F2}");
             Console.WriteLine("=========================");
-            Console.WriteLine("\n");
+            Console.WriteLine("\n");    // --> Console.WriteLine();
             Console.WriteLine("Thank you for your purchase, come again..");
             Console.WriteLine("Press any key to exit");
         }
